a2ec902ccc683cee3acee69b9853561dc57b1e8e
David Guerrero <david.guerrero@typeform.com>
2025-02-05 22:05:53 +0100

    Adding new protobuf messages for decoded ADSB messages

--- libs/python/adsb-protobufs/adsb_protobufs/protocols/adsb_pb2.py
+++ libs/python/adsb-protobufs/adsb_protobufs/protocols/adsb_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14protocols/adsb.proto\"2\n\tADSBFrame\x12\x11\n\tsource_id\x18\x01 \x01(\r\x12\x12\n\nframe_data\x18\x02 \x01(\x0c\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14protocols/adsb.proto\"2\n\tADSBFrame\x12\x11\n\tsource_id\x18\x01 \x01(\r\x12\x12\n\nframe_data\x18\x02 \x01(\x0c\"J\n\x10\x41irbornePosition\x12\x11\n\tsource_id\x18\x01 \x01(\r\x12\x10\n\x08latitude\x18\x02 \x01(\x01\x12\x11\n\tlongitude\x18\x03 \x01(\x01\"\xda\x01\n\x10\x41irborneVelocity\x12\x11\n\tsource_id\x18\x01 \x01(\r\x12\r\n\x05speed\x18\x02 \x01(\r\x12\x0f\n\x07heading\x18\x03 \x01(\x02\x12\x16\n\x0evertical_speed\x18\x04 \x01(\x11\x12/\n\nspeed_type\x18\x05 \x01(\x0e\x32\x1b.AirborneVelocity.SpeedType\"J\n\tSpeedType\x12\x10\n\x0cGROUND_SPEED\x10\x00\x12\x17\n\x13INDICATED_AIR_SPEED\x10\x01\x12\x12\n\x0eTRUE_AIR_SPEED\x10\x02\"P\n\x16\x41ircraftIdentification\x12\x11\n\tsource_id\x18\x01 \x01(\r\x12\x11\n\tcall_sign\x18\x02 \x01(\t\x12\x10\n\x08\x63\x61tegory\x18\x03 \x01(\rb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -33,4 +33,12 @@ if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_ADSBFRAME']._serialized_start=24
   _globals['_ADSBFRAME']._serialized_end=74
+  _globals['_AIRBORNEPOSITION']._serialized_start=76
+  _globals['_AIRBORNEPOSITION']._serialized_end=150
+  _globals['_AIRBORNEVELOCITY']._serialized_start=153
+  _globals['_AIRBORNEVELOCITY']._serialized_end=371
+  _globals['_AIRBORNEVELOCITY_SPEEDTYPE']._serialized_start=297
+  _globals['_AIRBORNEVELOCITY_SPEEDTYPE']._serialized_end=371
+  _globals['_AIRCRAFTIDENTIFICATION']._serialized_start=373
+  _globals['_AIRCRAFTIDENTIFICATION']._serialized_end=453
 # @@protoc_insertion_point(module_scope)
